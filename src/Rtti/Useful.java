303bf390e9c491cc8eb035c84d8dfb1b8b3e0a74
刘浩 <liuhao192@live.com>
2019-03-03 23:52:48 +0800

    向上造型的测试

---
+++ src/Rtti/Useful.java
@@ -0,0 +1,55 @@
+package Rtti;
+
+/**
+ * @author: LiuHao
+ * @Date: 2019/3/3
+ * @Time: 23:39
+ * @Description: LiuHao 2019/3/3 23:39
+ * 向上造型
+ */
+public class Useful {
+         public void f (){
+             System.out.println("Useful F");
+         };
+         public void g (){
+             System.out.println("Useful G");
+         };
+
+    public void upUseful(Useful u) {
+        System.out.println("upUseful");
+    }
+
+    public Useful() {
+    }
+
+    public static void main(String[] args) {
+        Useful[] x={
+                new  Useful(),
+                new  MoreUseful()
+        };
+
+        x[0].f();
+        x[1].f();
+        x[0].upUseful(x[1]);
+        ((MoreUseful) x[1]).u();
+        //向下造型异常错误
+        ((MoreUseful) x[0]).u();
+    }
+}
+
+class  MoreUseful extends Useful{
+    @Override
+    public void f (){
+        System.out.println("MoreUseful f");
+    };
+    @Override
+    public void g (){
+        System.out.println("MoreUseful g");
+    };
+    public void u (){
+        System.out.println("MoreUseful u");
+    };
+    public void v (){};
+    public void m (){};
+
+}
